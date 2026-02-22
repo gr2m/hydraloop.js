6c3d23cf6df98ebc99ed428db0fd8bce665f6452
Gregor Martynus <39992+gr2m@users.noreply.github.com>
2026-02-22 14:47:48 -0800

    test: enforce 100% coverage

--- vitest.config.js
+++ vitest.config.js
@@ -5,6 +5,12 @@ export default defineConfig({
     resolveSnapshotPath: (testPath, snapExtension) => testPath + snapExtension,
     coverage: {
       include: ["index.js"],
+      thresholds: {
+        lines: 100,
+        functions: 100,
+        branches: 100,
+        statements: 100,
+      },
     },
     typecheck: {
       include: ["test/types.test.ts"],
